fa3378952f76ede6bb017f41c3892a342c3ff662
natron19 <nathan19@hotmail.com>
2015-12-22 14:54:19 -0800

    complete authentication system

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
   #post 'users', to: 'users#create'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   resources :users, except: [:new]
 
