1ddb66463db29cb2ed3ceb9a33dbf423de8308ef
natron19 <nathan19@hotmail.com>
2015-12-22 10:01:33 -0800

    add user validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ActiveRecord::Base
 
 
   validates :title, presence: true, length: {minimum: 3, maximum: 50}
-  validates :description, presence: true,  length: {minimum: 10, maximum: 300}
+  validates :description, presence: true,  length: {minimum: 10, maximum: 800}
 
 
 end
